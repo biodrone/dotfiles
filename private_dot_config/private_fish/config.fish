67ba027c8ef1765430771a3b2dfe120505e34ef5
josh <josh@syndicated.solutions>
2022-08-17 10:49:47 +0100

    Initial new fish config

---
+++ private_dot_config/private_fish/config.fish
@@ -0,0 +1,17 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+## Fix SSH Agent
+fish_ssh_agent
+
+## Aliases
+alias dcup="docker-compose up -d"
+alias dcpull="docker-compose pull"
+alias dcdown="docker-compose down"
+alias dcbuild="docker-compose build"
+alias dcrebuild="docker-compose down && docker-compose up -d"
+alias dclogs="docker-compose logs -f"
+alias l="ls -lah"
+
+export EDITOR=nvim
