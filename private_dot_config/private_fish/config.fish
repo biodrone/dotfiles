73887c1b5b2e8f9f75e3824c1ef89e122c072e6d
Josh Jacobs <josh@joshjacobs.net>
2023-10-05 20:28:25 +0000

    Update for new docker compose without the dash

--- private_dot_config/private_fish/config.fish
+++ private_dot_config/private_fish/config.fish
@@ -6,12 +6,12 @@ end
 fish_ssh_agent
 
 ## Aliases
-alias dcup="docker-compose up -d"
-alias dcpull="docker-compose pull"
-alias dcdown="docker-compose down"
-alias dcbuild="docker-compose build"
-alias dcrebuild="docker-compose down && docker-compose up -d"
-alias dclogs="docker-compose logs -f"
+alias dcup="docker compose up -d"
+alias dcpull="docker compose pull"
+alias dcdown="docker compose down"
+alias dcbuild="docker compose build"
+alias dcrebuild="docker compose down && docker-compose up -d"
+alias dclogs="docker compose logs -f"
 alias l="ls -lah"
 alias code=code-insiders
 alias tf=terraform
