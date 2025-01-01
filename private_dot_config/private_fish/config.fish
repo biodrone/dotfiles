ea6b7ef1fb5cc46e766fff71958f25098664476b
Josh Jacobs <josh.jacobs@datasolace.com>
2025-01-01 14:42:14 +1000

    fix: add brew eval for macos

--- private_dot_config/private_fish/config.fish
+++ private_dot_config/private_fish/config.fish
@@ -29,3 +29,6 @@ set -g theme_date_timezone Europe/London
 set -g theme_newline_cursor yes
 set -g fish_prompt_pwd_dir_length 0
 set -g theme_title_use_abbreviated_path no
+
+# Homebrew Fix on MacOS
+eval "$(/opt/homebrew/bin/brew shellenv)"
